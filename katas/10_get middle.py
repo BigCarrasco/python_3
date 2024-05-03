5dd4cf5d076234755cfc1a1e2fe80865abbd0f33
Bigcarrasco <m.carrasco94@gmail.com>
2024-05-03 15:37:34 -0600

    ordered by number asc

--- katas/10_get middle.py
+++ katas/10_get middle.py
@@ -16,4 +16,9 @@ Kata.getMiddle("A") should return "A"
 '''
 def get_middle(s):
     index, odd = divmod(len(s), 2)
-    return s[index] if odd else s[index - 1:index + 1]
+    print(s[index] if odd else s[index - 1:index + 1])
+
+get_middle("test")
+get_middle("testing")
+get_middle("middle") 
+get_middle("A") 
